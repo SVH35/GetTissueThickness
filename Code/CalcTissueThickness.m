25f29d40402e426a6ac0165cad996c431078c0a5
Sybren Van Hoornweder <88398607+SVH35@users.noreply.github.com>
2022-05-05 09:30:44 +0200

    Update CalcTissueThickness.m

--- Code/CalcTissueThickness.m
+++ Code/CalcTissueThickness.m
@@ -1,41 +1,63 @@
 function [Output] = CalcTissueThickness(segDir, CoordOfInterest, ToPlot, nrOfPoints)
 
-tic
-% coordinates of interest should be in MNI space
-% Set the optional variables if omitted
+%% GTT
+%% General information
+% This script requires 3 mandatory inputs and 1 non-mandatory input.
 
-if nargin < 4
+% Input 1: the directory containing the segmented tissues (.stl) format.
+% Typically, this directory ends with 'm2m_SubjectName'.
+
+% Input 2: Coordinates of interest in MNI space. For instance, M1
+% coordinates are [−37 −21	58].
+
+% Input 3: Plot results or not? If == 1, results are plotted.
+
+% Input 4: Number of points used to create grey matter plane of best fit.
+% Per default, 5000 points are used.
+
+%% Code
+tic
+
+if nargin < 4 %number of points used if no input is supplied by user
     nrOfPoints = 5000;
+end
+
+if contains(struct2array(ver), 'Statistics and Machine Learning Toolbox') == 1 %checks if required toolboxes are installed
+    if contains(struct2array(ver), 'Parallel Computing Toolbox') == 1 %checks if recommended toolboxes are installed
+    else
+        disp('Parallel Computing Toolbox is not installed, considering installing this toolbox to run GTT faster');
+    end
+else
+    error('ERROR: Statistics and Machine Learning Toolbox is required to run this function but is not installed')
 end
 
 parentDir = fileparts(segDir);
 
+disp('Loading in tissues'); %loads in segmented tissues (triangulation matrices)
 skin = stlread([segDir filesep 'skin.stl']);
 bone = stlread([segDir filesep 'bone.stl']);
 csf  = stlread([segDir filesep 'csf.stl']);
 gm   = stlread([segDir filesep 'gm.stl']);
 wm   = stlread([segDir filesep 'wm.stl']);
 
-ROI_coord = mni2subject_coords(CoordOfInterest, segDir, 'nonl');
-gm1 = abs(gm.Points - ROI_coord);
-gmtotal = sum(gm1,2);  %Sum over dimension 2 (= horizontal)
+ROI_coord = mni2subject_coords(CoordOfInterest, segDir, 'nonl'); %transforms MNI coordinate to subject space, part of SimNIBS
 
+gm1 = abs(gm.Points - ROI_coord); %finds 3D grey matter point closest to subject space coordinate of interest and uses this for further computations
+gmtotal = sum(gm1,2);  
 [M,I] = min(gmtotal);
 m1coord_mesh = gm.Points(I,:);
 
-% create perpendicular line on ROI centre
-gm_plane = abs(gm.Points-m1coord_mesh);
-
+disp('Creating plane best fitting grey matter ROI and its normal'); 
+gm_plane = abs(gm.Points-m1coord_mesh); %find plane of best fit based on k-nearest grey matter points to coordinate of interest
 gmtotal_plane = sum(gm_plane,2);
 gmtotal = sum(gm1,2);
 gmtotal(:,2) = 1:length(gmtotal);
 gmtotal = sortrows(gmtotal,1,'ascend');
-idx = gmtotal(1:nrOfPoints,2); %max value is amount of points used to calculate plane normal
+idx = gmtotal(1:nrOfPoints,2); 
 gm_plane1 = gm.Points(idx,:);
-
 N = pca(gm_plane1);
-N = N(:,3);
 
+N = N(:,3); %normal of the plane of best fit (i.e., normal of grey matter plane)
 if (-1<CoordOfInterest(1)) && (CoordOfInterest(1)<1) %if ROI is on the midline, ensure that normal is always going upwards (i.e., outwards)
     if N(3)<=0
         N = N * -1;
@@ -46,13 +68,13 @@ else
     end
 end
 
-%% get correct vertices
+disp('Calculating tissue thicknesses');
+
 % tissue gm
 OutputGM = Triangulation2Intersect(gm, m1coord_mesh, N);
-
 [~,index] = sortrows([OutputGM.intersect].'); OutputGM = OutputGM(index(end:-1:1)); clear index
 outputGM_T = struct2table(OutputGM);
-if sum(outputGM_T{:,1}) == 1
+if sum(outputGM_T{:,1}) == 1 %take outer grey matter voxel for further distance calculations
     Output_GM{1} = OutputGM(1);
     intersectpoint_GM = OutputGM(1).xcoor;
 else
@@ -66,26 +88,22 @@ end
 OutputCSF = Triangulation2Intersect(csf, intersectpoint_GM, N);
 CSF_thickness = tissue_thickness(OutputCSF);
 CSF_Thickness(1) = CSF_thickness{1}.t;
-
 % get bone thickness
 OutputBone = Triangulation2Intersect(bone, intersectpoint_GM, N);
 bone_thickness = tissue_thickness(OutputBone);
 Bone2GM(1) = bone_thickness{1}.t;
 Bone_Thickness(1) = Bone2GM(1) - CSF_Thickness(1);
-
 % get skin thickness
 OutputSkin = Triangulation2Intersect(skin, intersectpoint_GM, N);
 skin_thickness = tissue_thickness(OutputSkin);
 Skin2GM(1) = skin_thickness{1}.t;
 Skin_Thickness(1) = Skin2GM(1) - (CSF_Thickness(1) + Bone_Thickness(1));
-
 % get scalp to cortex distance
 ScalpToCortex = CSF_Thickness(1) + Bone_Thickness(1) + Skin_Thickness(1);
 
-%% create output file
-Output = table(CSF_Thickness,Bone_Thickness,Skin_Thickness,ScalpToCortex);
+Output = table(CSF_Thickness,Bone_Thickness,Skin_Thickness,ScalpToCortex); %create output file
 
-if ToPlot == 1
+if ToPlot == 1 %plot results
     quiver3(m1coord_mesh(1),m1coord_mesh(2),m1coord_mesh(3),(N(1)*60),(N(2)*60),(N(3)*60),'Color','r')
     hold on
     plot3(intersectpoint_GM(1),intersectpoint_GM(2),intersectpoint_GM(3),'o','Color','#ff0000','MarkerSize',10,'MarkerFaceColor','#ff0000')
@@ -104,14 +122,10 @@ if ToPlot == 1
     hold on
     plot3(gm.Points(:,1),gm.Points(:,2),gm.Points(:,3),'o','Color','#949494','MarkerSize',3,'MarkerFaceColor','#949494')
     hold on
-    hold on
-    w = null(N.'); % Find two orthonormal vectors which are orthogonal to v
-    [P,Q] = meshgrid(-15:15); % Provide a gridwork (you choose the size)
-    X = m1coord_mesh(1)+w(1,1)*P+w(1,2)*Q; % Compute the corresponding cartesian coordinates
-    Y = m1coord_mesh(2)+w(2,1)*P+w(2,2)*Q; %   using the two vectors in w
-    Z = m1coord_mesh(3)+w(3,1)*P+w(3,2)*Q;
-    surf(X,Y,Z)
+    w = null(N.'); 
+    [P,Q] = meshgrid(-15:15); 
+    coords = [m1coord_mesh(1)+w(1,1)*P+w(1,2)*Q, m1coord_mesh(2)+w(2,1)*P+w(2,2)*Q, m1coord_mesh(3)+w(3,1)*P+w(3,2)*Q];
+    surf(coords(1:31,1:31),coords(1:31,32:62),coords(1:31,63:93))
 end
-
 toc
 end
